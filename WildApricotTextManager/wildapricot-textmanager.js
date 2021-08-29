8393ba066648ad3e7a63ac82110877dbed8bda91
Matthieu Disna <info@mdisna.dev>
2021-08-29 10:38:19 -0400

    Hide language toggle button if page loaded in widget mode

--- WildApricotTextManager/wildapricot-textmanager.js
+++ WildApricotTextManager/wildapricot-textmanager.js
@@ -61,13 +61,21 @@ $(document).ready(function () {
   if (typeof inspectorLocation === "undefined") {
     inspectorLocation = "bottom";
   }
+
+  // Hide language toggle button if page loaded as a widget
+  if (window.location.href.indexOf("/widget/") > -1) {
+    hideToggle = true;
+  } else {
+    hideToggle = false;
+  }
+
   // Start Inspector if keyword present
   if (window.location.href.indexOf(inspectorKeword) > -1) {
     startDev();
   }
 
   // Multiligual Mode
-  if (textManagerMultilingualMode) {
+  if (textManagerMultilingualMode & !hideToggle) {
     if ($("#languageButton").length) {
       // Use Content Gadget if exists
       $("#languageButton").append('<div><button type="button" id="languageToggle">Toggle Second Language</button></div>');
@@ -186,7 +194,7 @@ function getCookie(key) {
 }
 
 function isInEditMode() {
-  // detect if we are in admin view
+  // detect if we are in admin
   return $("body").hasClass("adminContentView");
 }
 
