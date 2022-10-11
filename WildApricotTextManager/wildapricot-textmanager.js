73ab8f9da1e8e49e8e3b4344ec04c9346a97e2f1
mDisna <mdisna@gmail.com>
2022-10-11 18:11:19 -0400

    Update FA macro function to run after  WATM text replacements

--- WildApricotTextManager/wildapricot-textmanager.js
+++ WildApricotTextManager/wildapricot-textmanager.js
@@ -242,6 +242,20 @@ function start(license) {
                   log(e, "Error");
                 }
               }
+              // FontAwesome
+              document.querySelectorAll("body *").forEach(function (el) {
+                let regex = /\[ez-fa]{1,}(.*?)\[\/ez-fa]{1,}/gi;
+                let faRegex = /(?<=\[ez-fa])([a-z0-9].*?)(?=\[\/ez-fa])/gi;
+                walkText(el, regex, "icon", function (node, match, offset) {
+                  let iconEl = document.createElement("i");
+                  iconEl.classList.add("fa-solid");
+                  while ((faIcon = faRegex.exec(match)) !== null) {
+                    if (faIcon !== undefined)
+                      iconEl.classList.add(`fa-${faIcon[0]}`);
+                  }
+                  return iconEl;
+                });
+              });
             },
             error: () => {
               log(`"${currentCSV}" not found`, "Error");
@@ -250,20 +264,6 @@ function start(license) {
         );
       }
     };
-
-    // FontAwesome
-    document.querySelectorAll("body *").forEach(function (el) {
-      let regex = /\[ez-fa]{1,}(.*?)\[\/ez-fa]{1,}/gi;
-      let faRegex = /(?<=\[ez-fa])([a-z0-9].*?)(?=\[\/ez-fa])/gi;
-      walkText(el, regex, "icon", function (node, match, offset) {
-        let iconEl = document.createElement("i");
-        iconEl.classList.add("fa-solid");
-        while ((faIcon = faRegex.exec(match)) !== null) {
-          if (faIcon !== undefined) iconEl.classList.add(`fa-${faIcon[0]}`);
-        }
-        return iconEl;
-      });
-    });
   }
 
   const showWATMIcon = (license) => {
