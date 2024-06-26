b7e2225d6549e0065be1d2478cc3e2bec702c2b0
mDisna <mdisna@gmail.com>
2024-06-26 12:48:03 -0400

    Rework EZ_Language to hande titles and descriptions differently
    #46

--- WildApricotTextManager/ez-addons/ez-language.js
+++ WildApricotTextManager/ez-addons/ez-language.js
@@ -2,50 +2,43 @@ let ez_language_addon_version = "1.0";
 log(`EZ-Language Addon Version ${ez_language_addon_version} enabled.`);
 
 let ez_language = () => {
-  const classesToProcess = [
-    "WaGadgetEvents",
-    "WaGadgetUpcomingEvents",
+  const classesToProcessSystem = [
+    "pageTitleOuterContainer",
     "WaGadgetBreadcrumbs",
+    "eventDetailsLink",
+    "pastEventLink",
+    "eventDivItem",
     "OnlineStoreCatalog_list_item_link",
     "OnlineStoreProduct_title_container",
-    "OnlineStoreProduct_description",
-    "storeCartTable_itemTitle",
     "storeCartTable_tdTitle",
+    "storeCartTable_itemTitle",
+  ];
+  const classesToProcessUser = [
+    "gadgetEventEditableArea",
+    "OnlineStoreProduct_description",
   ];
 
+  const classesToProcess = [...classesToProcessSystem, ...classesToProcessUser];
+
+  const processHtml = (html, className) => {
+    const regex = /\[ez\s+(\w+)\](.*?)\[\/ez\]/gs;
+
+    return html.replace(regex, (match, p1, p2) => {
+      const displayStyle = `style="display: ${
+        p1 === currentLanguage ? "inline" : "none"
+      };"`;
+      const classAddedContent = p2.replace(
+        /(<\w+)([^>]*>)/g,
+        `$1 class="${p1}" ${displayStyle} $2`
+      );
+      return `<div class="${p1}" ${displayStyle}>${classAddedContent}</div>`;
+    });
+  };
+
   classesToProcess.forEach((className) => {
-    const elements = document.querySelectorAll(`.${className}`);
-
-    elements.forEach((element) => {
-      let html = element.innerHTML;
-
-      html = html.replace(
-        /(<(?!div)[^>]+>)?\[ez ([^\]]+)\]/gi,
-        function (match, precedingTag, p1) {
-          let spanTag = `<span class="${p1.trim()}"`;
-          if (p1.trim() === currentLanguage) {
-            spanTag += ">";
-          } else {
-            spanTag += ' style="display:none;">';
-          }
-          if (precedingTag) {
-            return spanTag + precedingTag;
-          }
-          return spanTag;
-        }
-      );
-
-      html = html.replace(
-        /(<(?!div)[^>]+>)?\[\/ez\]/gi,
-        function (match, precedingTag) {
-          if (precedingTag) {
-            return `</span>${precedingTag}`;
-          }
-          return `</span>`;
-        }
-      );
-
-      element.innerHTML = html;
+    const containers = document.querySelectorAll(`.${className}`);
+    containers.forEach((container) => {
+      container.innerHTML = processHtml(container.innerHTML, className);
     });
   });
 };
